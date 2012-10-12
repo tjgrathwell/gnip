2d8d2b93f9e89e2a6d79474c8f2856b01d311fca
Brandon Liu & Callum Stott <pair+brandon+callum@pivotallabs.com>
2012-10-12 11:38:51 -0700

    Add to_result method to turn a gnip url into a result object with csv contents, header and types

--- lib/chorusgnip.rb
+++ lib/chorusgnip.rb
@@ -1,5 +1,9 @@
+require 'net/http'
 require 'net/https'
 require "uri"
+require 'csv'
+require 'zlib'
+require 'json'
 
 class ChorusGnip
 
@@ -9,26 +13,118 @@ class ChorusGnip
     @url= input_values[:url]
     @username= input_values[:username]
     @password= input_values[:password]
+
+    @uri = URI.parse(@url)
+    raise Exception, "URI not valid" unless @uri.host == 'historical.gnip.com'
+    raise Exception, "URI not valid" unless @uri.scheme == 'https'
+  end
+
+  def create_connection
+    http = Net::HTTP.new(@uri.host, @uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    http
   end
 
   def auth
-    begin
-      uri = URI.parse(@url)
-    rescue Exception => e
-      return false
-    end
+    http = create_connection
 
-    return false unless uri.host == 'historical.gnip.com'
-    return false unless uri.scheme == 'https'
-
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-    request = Net::HTTP::Head.new(uri.request_uri)
+    request = Net::HTTP::Head.new(@uri.request_uri)
     request.basic_auth(@username, @password)
     response = http.request(request)
 
     response.code == "200"
+  end
+
+  def fetch
+    http = create_connection
+    request = Net::HTTP::Get.new(@uri.request_uri)
+    request.basic_auth(@username, @password)
+    response = http.request(request)
+    CSV.parse(response.body, { :col_sep => "\t", :quote_char => "'" }).map { |row| row.last }
+  end
+
+  def to_result
+    resources_urls = fetch
+
+    csv_string = CSV.generate(:force_quotes => true) do |csv|
+      resources_urls.each do |value|
+        list_of_hashes = GnipJson.new(:url => value).parse.each do |hsh|
+          csv << [hsh['id'], hsh['body']]
+        end
+      end
+    end
+
+    GnipCsvResult.new(csv_string)
+
+
+      # #'message_id'
+      #       tweet_row << hsh['id']
+      #       #'spam'
+      #       tweet_row << nil
+      #       #'created_at'
+      #       tweet_row << hsh['postedTime']
+      #       #'source'
+      #       tweet_row << nil
+      #       #'retweeted'
+      #       tweet_row << hsh['retweetCount']
+      #       #'favorited'
+      #       tweet_row << nil
+      #       #'truncated'
+      #       tweet_row << nil
+      #       #'in_reply_to_screen_name',
+      #       tweet_row <<
+      #       #'in_reply_to_user_id',
+      #       #'author_id',
+      #       #'author_name',
+      #       #'author_screen_name',
+      #       #'author_lang',
+      #       #'author_url',
+      #       #'author_description',
+      #       #'author_listed_count',
+      #       #'author_statuses_count',
+      #       #'author_followers_count',
+      #       #'author_friends_count',
+      #       #'author_created_at',
+      #       #'author_location',
+      #       #'author_verified',
+      #       #'message_url',
+      #       #'message_text'
+      #
+
+
+      # Append it into a file
+  end
+end
+
+class GnipJson
+  attr_reader :url
+
+  def initialize(options)
+    @url = options[:url]
+  end
+
+  def parse
+    uri = URI.parse(url)
+
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Get.new(uri.request_uri)
+    response = http.request(request)
+    gz = Zlib::GzipReader.new(StringIO.new(response.body))
+    gz.read.split("\n").reject{ |e| e.match(/^\s*$/) }[0...-1].inject([]) { |a, value| a << JSON.parse(value) }
+  end
+end
+
+class GnipCsvResult
+  attr_reader :column_names
+  attr_reader :types
+  attr_reader :contents
+
+  def initialize(contents)
+    @column_names = ['id', 'body']
+    @types = ['text', 'text']
+    @contents = contents
   end
 end
--- spec/chorusgnip_spec.rb
+++ spec/chorusgnip_spec.rb
@@ -1,6 +1,7 @@
 require_relative '../lib/chorusgnip'
 
 require 'vcr'
+require 'csv'
 
 REAL_STREAM_URL=''
 REAL_USERNAME=''
@@ -38,36 +39,75 @@ describe 'Chorusgnip' do
     end
 
     it "returns false for a plain HTTP url" do
-      VCR.use_cassette('plain_http_gnip') do
+      expect do
         g = ChorusGnip.new(
-          :url => 'http://historical.gnip.com/something',
-          :username => 'someemail',
-          :password => 'wrongpassword')
-
-        g.auth.should be_false
-      end
+        :url => 'http://historical.gnip.com/foo',
+        :username => 'someemail',
+        :password => 'wrongpassword')
+      end.to raise_exception
     end
 
     it "returns false for a url that cannot be parsed" do
-      VCR.use_cassette('invalid') do
-        g = ChorusGnip.new(
+        expect do
+          g = ChorusGnip.new(
           :url => nil,
           :username => 'someemail',
           :password => 'wrongpassword')
-
-        g.auth.should be_false
-      end
+        end.to raise_exception
     end
 
     it "makes sure that the URL is for a Gnip stream and not say https://google.com" do
-      VCR.use_cassette('not_a_gnip_stream') do
+      expect do
         g = ChorusGnip.new(
-          :url => 'https://www.google.com',
-          :username => 'something',
-          :password => 'wrongpassword')
+        :url => 'https://www.google.com',
+        :username => 'someemail',
+        :password => 'wrongpassword')
+      end.to raise_exception
+    end
+  end
 
-        g.auth.should be_false
+  context "producing a valid CSV file" do
+    it "gets all the historical data" do
+      VCR.use_cassette('successful_get') do
+        g = ChorusGnip.new(
+          :url => REAL_STREAM_URL,
+          :username => REAL_USERNAME,
+          :password => REAL_PASSWORD)
+
+        results = g.fetch
+        results.length.should_not == 0
+        results.each { |url| url.should include('https:') }
+
+        result = g.to_result
+        result.class.should == GnipCsvResult
+        csv = CSV.parse(result.contents)
+        csv.length.should == 42
+        csv.each do |row|
+          row.length.should == 2
+        end
+        result.column_names.should == ['id', 'body']
+        result.types.should == ['text', 'text']
+
+        # result.column_names.should == [
+        #   'message_id', 'spam', 'created_at', 'source', 'retweeted', 'favorited', 'truncated', 'in_reply_to_screen_name',
+        #   'in_reply_to_user_id', 'author_id', 'author_name', 'author_screen_name', 'author_lang', 'author_url', 'author_description',
+        #   'author_listed_count', 'author_statuses_count', 'author_followers_count', 'author_friends_count', 'author_created_at',
+        #   'author_location', 'author_verified', 'message_url', 'message_text'
+        # ]
+        # result.types.should == [
+        #   'bigint', 'boolean', 'timestamp without time zone', 'text', 'boolean', 'boolean', 'boolean', 'text', 'bigint',
+        #   'bigint', 'text', 'text', 'text', 'text', 'text', 'integer', 'integer', 'integer', 'integer', 'timestamp without time zone', 'text', 'boolean', 'text', 'text'
+        #   ]
       end
+    end
+  end
+
+  context "operations on a single JSON file" do
+    VCR.use_cassette('3_activity_json') do
+      c = GnipJson.new(:url => REAL_GNIP_JSON_URL)
+      activity_list = c.parse
+      activity_list.length.should == 3
+      activity_list.each { |hash| hash['id'].should_not == nil }
     end
   end
 end
